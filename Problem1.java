59a89223f27399cf7cf9c596a960c7d063542bd5
Choudhary Farukh <119169306+ChFarukh1@users.noreply.github.com>
2024-08-07 20:40:20 +0500

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("It is palindrome");
+        } else {
+            System.out.println("It is not palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
