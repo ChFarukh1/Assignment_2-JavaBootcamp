aef44a1f44cfdfde8e8cbf8a1db95325c7ec5c61
Choudhary Farukh <119169306+ChFarukh1@users.noreply.github.com>
2024-08-07 20:41:53 +0500

    Update Problem4.java

--- Problem4.java
+++ Problem4.java
@@ -1,1 +1,13 @@
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class AddDaysToDate {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.parse("2024-07-24");
+        LocalDate newDate = date.plusDays(30);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = formatter.format(newDate);
+        System.out.println("New Date after adding 30 days: " + formattedDate);
+    }
+}
