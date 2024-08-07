7b52f03d6e401a721c1e8a616be688b33b3a6283
Choudhary Farukh <119169306+ChFarukh1@users.noreply.github.com>
2024-08-07 20:40:53 +0500

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,21 @@
 
+import java.util.Scanner;
+
+public class ValidateEmail {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an email address: ");
+        String email = scanner.nextLine();
+
+        if (isValidUMTEmail(email)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+    public static boolean isValidUMTEmail(String email) {
+        String umtRegex = "^(\\d{10}|[a-zA-Z]+\\.\\w+)@gmail\\.com$";
+        return email.matches(umtRegex);
+    }
+}
