30d7349723f823f4c48523604ac060379740a2f2
Choudhary Farukh <119169306+ChFarukh1@users.noreply.github.com>
2024-08-07 20:41:13 +0500

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class FormatDate {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.parse("2024-07-24");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-M-yyyy");
+        String formattedDate = formatter.format(date);
+        System.out.println("Formatted Date: " + formattedDate);
+    }
+}
